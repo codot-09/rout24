ee6f1cc0ed6edcdfd64935469151c4d9268a9709
codot-09 <onabiyev626@gmail.com>
2025-12-07 11:04:04 +0500

    nimadir

--- src/main/java/com/example/rout24/config/SecurityConfig.java
+++ src/main/java/com/example/rout24/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
+        config.setExposedHeaders(List.of("Authorization"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
